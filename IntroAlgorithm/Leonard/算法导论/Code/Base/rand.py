649d3c85152ed08b56800a229bda47987ad6d3a0
Zhenduo Wang <wzdlc1996@gmail.com>
2021-04-19 23:49:42 +0800

    update rand

--- IntroAlgorithm/Leonard/算法导论/Code/Base/rand.py
+++ IntroAlgorithm/Leonard/算法导论/Code/Base/rand.py
@@ -2,13 +2,13 @@
 
 import random
 
-def randArr1D(leng: int, rang: slice) -> list:
+def randArr1D(leng: int, rang: slice = (-1,1)) -> list:
     """
     Generate a random 1-D array.
 
-    Keyword arguments:
-    leng:   Length of the array (non negative integer)
-    rang:   Range of the random variables (slice of size 2)
+    :param leng: Length of the array (non negative integer)
+    :param rang: Range of the random variables (slice of size 2), default (-1,1)
+    :return: 1-D array, with its elements randomly distributed in range.
     """
     if len(rang) == 0:
         rr = (-1,1)
@@ -16,7 +16,24 @@ def randArr1D(leng: int, rang: slice) -> list:
         rr = rang + (1,)
     else:
         rr = rang
-    arr = [0] * leng
-    for i in range(len(arr)):
-        arr[i] = random.randint(rr[0], rr[1])
+    arr = random.choices(range(rr[0], rr[1]), k=leng)
     return arr
+
+def randMatrix(m: int, n: int, rang: slice = (-1,1)) -> list:
+    """
+    Generate a random matrix. Its elements are independently evenly distributed in the range of rang.
+
+    :param m: the number of rows
+    :param n: the number of columes
+    :param rang: range of the random variables (slice of size 2), default (-1,1)
+    :return: 2-D array, as the matrix of m x n
+    """
+    if len(rang) == 0:
+        rr = (-1,1)
+    elif len(rang) == 1:
+        rr = rang + (1,)
+    else:
+        rr = rang
+    arr = [[0] * m] * n
+    for i in range(m):
+        arr[i] = randArr1D(n, rr)
