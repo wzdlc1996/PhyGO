90c55f5ddac964680ec5bfca42ddcb94ea759671
Zhenduo Wang <wzdlc1996@gmail.com>
2021-06-23 00:06:25 +0800

    complete binsearchtree test

--- IntroAlgorithm/Leonard/算法导论/Code/Chap_12/binSearchTree.py
+++ IntroAlgorithm/Leonard/算法导论/Code/Chap_12/binSearchTree.py
@@ -18,6 +18,8 @@ class binSearchTree:
         self.leftInd = []
         self.rightInd = []
         self.pInd = []
+        self.root = None
+        self.size = 0
     
     def left(self, x):
         return self.leftInd[x]
@@ -30,6 +32,15 @@ class binSearchTree:
 
     def isEmpty(self, x):
         return x is None
+
+    def getRoot(self):
+        return self.root
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, k):
+        return self.body[k]
 
     def inorderTreeWalk(self, x):
         if not self.isEmpty(x):
@@ -44,33 +55,42 @@ class binSearchTree:
         :param size: tree size
         """
         x = list(range(size))
-        self.body = list(range(size))
+        self.size = size
+        self.body = list(range(5,5 + size))
         self.leftInd = [None] * size
         self.rightInd = [None] * size
         self.pInd = [None] * size
+        rind = int(size / 2)
+        self.root = x[rind]
 
-        def binize(indlist, rt, mode, sf):
-            size = len(indlist)
-            if size == 0:
+        def binize(indlist, rt, mode):
+            """
+            Assign indlist into self with rt as the root, referred by mode as left or right subtree
+
+            :param indlist: index list, subset of x
+            :param rt: root index
+            :param mode: 0/1 valued, 0 means left subtree, 1 means right subtree
+            """
+            sz = len(indlist)
+            if sz == 0:
                 return
-            elif size == 1:
-                x = indlist[0]
+            else:
+                hf = int(sz / 2)
+                r = indlist[hf]
+                self.pInd[r] = rt
                 if mode == 0:
-                    sf.leftInd[rt] = x
+                    self.leftInd[rt] = r
                 else:
-                    sf.rightInd[rt] = x
-                sf.pInd[x] = rt
-                return
-
-            hf = int(size / 2)
-            binize(indlist[:hf], hf, 0, sf)
-            binize(indlist[hf+1:], hf, 1, sf)
+                    self.rightInd[rt] = r
+                binize(indlist[:hf], r, 0)
+                binize(indlist[hf+1:], r, 1)
         
-        binize(x, x[int(size / 2)], 0, self)
+        binize(x[:rind], self.root, 0)
+        binize(x[rind+1:], self.root, 1)
 
     def __str__(self):
         return "Tree size: {}\nL list: {}\nR list: {}".format(
-            len(self.body),
+            self.size,
             str(self.leftInd),
             str(self.rightInd)
         )
